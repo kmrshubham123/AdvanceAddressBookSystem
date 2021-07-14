8c44037f1243804fd1ba0cd429facbd1654a6d01
kmrshubham123 <kmrshubham123@gmail.com>
2021-07-14 23:14:54 +0530

    [Kumar Shubham]:Added:Address Book Table

--- AddressBookADO/AddressBookADO/AddressBook.cs
+++ AddressBookADO/AddressBookADO/AddressBook.cs
@@ -5,7 +5,14 @@ using System.Text;
 namespace AddressBookADO
 {
      public class AddressBook
-    {
-
-    }
+     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+     }
 }
---
+++ AddressBookTable.sql
@@ -0,0 +1,11 @@
+﻿--UC2-Create a Address Book Table with first and Last name,address,city,state,zip,PhoneNumber and email as its attributes
+Create Table AddressBook(
+FirstName varchar(25),
+LastName varchar(25),
+address varchar(50),
+City varchar(20),
+State varchar(20),
+Zip int,
+PhoneNumber varchar(15),
+Email varchar(50)
+);
