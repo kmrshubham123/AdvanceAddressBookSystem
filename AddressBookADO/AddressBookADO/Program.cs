6132b4bcc677dd25e9ad429084b2e15446bf15eb
kmrshubham123 <kmrshubham123@gmail.com>
2021-07-14 23:02:06 +0530

    [Kumar Shubham]Added:welcome to address book using ADO.net

---
+++ AddressBookADO/AddressBookADO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book contact data using ADO.Net");
+        }
+    }
+}
---
+++ AddressBookADO/CreateDataBase.sql
@@ -0,0 +1,3 @@
+﻿--UC1 Create a Address Book Service Database
+create database AddressBook_Service;
+use AddressBook_Service;
